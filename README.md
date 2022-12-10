ed70cf7533c751f3a6b9bbe9761c2e5ce9180c32
Apip <davitaja92@gmail.com>
2022-12-10 11:03:14 +0700

    create ts config

---
+++ README.md
@@ -0,0 +1,25 @@
+# TYPESCRIPT
+
+## 1. setup config
+
+## 2. kenapa typescript
+
+## 3. perbedaan javascript dan typescript
+
+## 4. konsep/cara kerja typescript
+
+## 5. tipe data
+
+## 6. typescript type
+
+## 7. array & tuples
+
+## 8. object
+
+## 9. function
+
+## 10. classes
+
+## 11. interface dan type
+
+## 12. advance type
---
+++ build/materi_1.js
@@ -0,0 +1,25 @@
+"use strict";
+// setup config :
+// 1. node
+//    node -v
+// 2. npm
+//    npm -v
+// 3. package.json
+//    npm init                            => mengenerate package.json file
+// 4. typescript
+//    npm install typescript --save-dev   => dev
+//    npm install -g typescript           => global
+// 5. nodemon
+//    npm install -g nodemon
+// 6. scripts
+//    "tsc"
+//    "tsc -w"                            => utk lihat hasil compile
+//    "nodemon ./build/index.js"          => utk run dev auto refresh
+//    "node ./build/index.js"             => utk run dev tidak auto refresh
+// 7. tsconfig.json
+//    tsc --init
+//           "allowJs": true
+//           "outDir": "./build"          => utk menyimpan hasil build ts -> js
+// 8. run dev
+//    tsc -w
+//    nodemon ./build/index.js
