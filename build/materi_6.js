968c8695bc22e26d27abdb3cbb6dc5ea3c2a70b6
Apip <davitaja92@gmail.com>
2022-12-11 22:53:15 +0700

    object

---
+++ build/materi_6.js
@@ -0,0 +1,63 @@
+"use strict";
+// Object
+// Object literal
+// menggunakan type inference
+let identitas = {
+    nama: "Danu",
+    umur: 23,
+    alamat: "Jakarta",
+};
+console.log(identitas);
+// menggunakan inline interface
+let identitas_2;
+identitas_2 = {
+    nama: "Danu",
+    umur: 23,
+    alamat: "Jakarta",
+};
+let identitas_3;
+let identitas_4;
+let identitas_5 = {
+    nama: "Danu",
+    umur: 23,
+    alamat: {
+        jalan: "sudirman",
+        kota: "jakarta selatan",
+        prov: "DKI Jakarta",
+    },
+};
+// exercise :
+// 1. buat nested, array of object menggunakan interface dan square brackets/generic type
+// output :
+// {
+//     id: "U-1",
+//     name: "Adi dodi",
+//     address: [
+//       {
+//         street : "Jln. Setapak No.2",
+//         city: "Jakarta"
+//       },
+//       {
+//         street: "Jln. Lebar sekali no 10",
+//         city: "Medan"
+//       }
+//     ]
+//  }
+// 2. buat nested, object of object
+// output :
+// {
+//     idCart: "C1",
+//     dateOrdered: "2020-05-20",
+//     items: {
+//       p1 :{
+//         "id": "P-1",
+//         "name": "Mechanical Keyboard",
+//         "qty": 2
+//       },
+//       p2 :{
+//         "id": "P-2",
+//         "name": "USB Hub",
+//         "qty": 1
+//       },
+//     }
+// }
---
+++ materi_6.ts
@@ -0,0 +1,88 @@
+// Object
+
+// Object literal
+// menggunakan type inference
+let identitas = {
+  nama: "Danu",
+  umur: 23,
+  alamat: "Jakarta",
+};
+console.log(identitas);
+
+// menggunakan inline interface
+let identitas_2: {
+  nama: string;
+  umur: number;
+  alamat: string;
+};
+identitas_2 = {
+  nama: "Danu",
+  umur: 23,
+  alamat: "Jakarta",
+};
+
+// menggunakan interface
+interface Identitas {
+  nama: string;
+  umur: number;
+  alamat: string;
+}
+let identitas_3: Identitas;
+let identitas_4: Identitas;
+
+// Nested object
+interface IdentitasNested {
+  nama: string;
+  umur: number;
+  alamat: Alamat;
+}
+interface Alamat {
+  jalan: string;
+  kota: string;
+  prov: string;
+}
+let identitas_5: IdentitasNested = {
+  nama: "Danu",
+  umur: 23,
+  alamat: {
+    jalan: "sudirman",
+    kota: "jakarta selatan",
+    prov: "DKI Jakarta",
+  },
+};
+
+// exercise :
+// 1. buat nested, array of object menggunakan interface dan square brackets/generic type
+// output :
+// {
+//     id: "U-1",
+//     name: "Adi dodi",
+//     address: [
+//       {
+//         street : "Jln. Setapak No.2",
+//         city: "Jakarta"
+//       },
+//       {
+//         street: "Jln. Lebar sekali no 10",
+//         city: "Medan"
+//       }
+//     ]
+//  }
+// 2. buat nested, object of object
+// output :
+// {
+//     idCart: "C1",
+//     dateOrdered: "2020-05-20",
+//     items: {
+//       p1 :{
+//         "id": "P-1",
+//         "name": "Mechanical Keyboard",
+//         "qty": 2
+//       },
+//       p2 :{
+//         "id": "P-2",
+//         "name": "USB Hub",
+//         "qty": 1
+//       },
+//     }
+// }
